9803d3717d6e02a9a9dfbe77916c12ce05f973d8
Jose Carlos <josealarconchacon@gmail.com>
2021-07-01 12:38:02 -0400

    compare user_answers

--- js/main.js
+++ js/main.js
@@ -7,7 +7,13 @@ form.addEventListener('submit',e => {
 
     // create score for the user as start answering the questions
     let user_score = 0;
-    const user_answers = [form.quest1,form.quest2,form.quest3,form.quest4,form.quest5];
+    const user_answers = [form.quest1.value,form.quest2.value,form.quest3.value,form.quest4.value,form.quest5.value];
     // compare user_answers to the correct_answers
-    
+    user_answers.forEach((answers, index) => {
+        if(answers === correct_answers[index]) {
+            user_score += 25;
+        }
+    });
+
+    console.log(user_score);
 });
